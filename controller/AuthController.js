26765db09dc2243708e1bfb264595bfe7125ba35
doguebart <douglasweelber.2004@outlook.com>
2023-05-12 17:28:33 -0300

    register route created, creating and styling flash messages

--- controller/AuthController.js
+++ controller/AuthController.js
@@ -1,15 +1,32 @@
+const User = require("../models/User");
+
+const bcrypt = require('bcryptjs');
+
 module.exports = class AuthController {
+  static login(req, res) {
+    res.render("auth/login");
+  }
 
-    static login(req, res) {
+  static register(req, res) {
+    res.render("auth/register");
+  }
 
+  static async registerSave(req, res) {
+    const { name, email, password, cfPassword } = req.body;
 
-        res.render('auth/login')
+    if (!name || !email || !password || !cfPassword) {
+      req.flash("message", "Preencha todos os campos antes de continuar!");
+      res.render("auth/register");
+
+      return;
     }
 
-    static register(req, res) {
+    // Password match validation
+    if (password != cfPassword) {
+      req.flash("message", "As senhas não coincidem!");
+      res.render("auth/register");
 
-
-        res.render('auth/register')
+      return;
     }
-
-}
+  }
+};
--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -5,5 +5,6 @@ const AuthController = require("../controller/AuthController");
 
 router.get("/login", AuthController.login);
 router.get("/register", AuthController.register);
+router.post("/register", AuthController.registerSave);
 
 module.exports = router;
