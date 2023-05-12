7d686a877637ebc1953902f5c677c70c6eb95da6
doguebart <douglasweelber.2004@outlook.com>
2023-05-12 18:12:56 -0300

    creating function to save user data at database and session persistence

--- controller/AuthController.js
+++ controller/AuthController.js
@@ -1,6 +1,6 @@
 const User = require("../models/User");
 
-const bcrypt = require('bcryptjs');
+const bcrypt = require("bcryptjs");
 
 module.exports = class AuthController {
   static login(req, res) {
@@ -27,6 +27,41 @@ module.exports = class AuthController {
       res.render("auth/register");
 
       return;
+    }
+
+    // Check if user exist
+    const checkIfUserExist = await User.findOne({ where: { email: email } });
+
+    if (checkIfUserExist) {
+      req.flash("message", "Este e-mail já está em uso!");
+      res.render("auth/register");
+
+      return;
+    }
+
+    // Create a password
+    const salt = bcrypt.genSaltSync(10);
+    const hashedPassword = bcrypt.hashSync(password, salt);
+
+    const user = {
+      name,
+      email,
+      password: hashedPassword,
+    };
+
+    try {
+      const createdUser  = await User.create(user);
+
+      // Initialize session
+      req.session.userid = createdUser.id;
+
+      req.flash("message", "Cadastro realizado com sucesso!");
+
+      req.session.save(() => {
+        res.redirect("/");
+      });
+    } catch (error) {
+      console.log(error);
     }
   }
 };
