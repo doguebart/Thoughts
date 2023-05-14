cba42004d0a2a7f72e8998fb4f1d72b6d1c3d902
doguebart <douglasweelber.2004@outlook.com>
2023-05-14 02:13:35 -0300

    creating thought form/route and sending data to database

--- controller/ThoughsController.js
+++ controller/ThoughsController.js
@@ -3,12 +3,33 @@ const User = require("../models/User");
 
 module.exports = class ThoughtsController {
   static async showThoughts(req, res) {
-    
     res.render("thoughts/home");
   }
 
   static async dashboard(req, res) {
-    
     res.render("thoughts/dashboard");
+  }
+
+  static createThought(req, res) {
+    res.render("thoughts/create");
+  }
+
+  static async createThoughtSave(req, res) {
+    const thought = {
+      title: req.body.title,
+      UserId: req.session.userid,
+    };
+
+    try {
+      await Thought.create(thought);
+
+      req.flash("message", "Pensamento criado com sucesso!");
+
+      req.session.save(() => {
+        res.redirect("/thoughts/dashboard");
+      });
+    } catch (error) {
+      console.log(error);
+    }
   }
 };
--- routes/thoughtsRoutes.js
+++ routes/thoughtsRoutes.js
@@ -5,6 +5,8 @@ const ThoughtsController = require("../controller/ThoughsController");
 // Helper
 const checkAuth = require("../helpers/auth").checkAuth;
 
+router.get("/add", checkAuth, ThoughtsController.createThought);
+router.post("/add", checkAuth, ThoughtsController.createThoughtSave);
 router.get("/dashboard", checkAuth, ThoughtsController.dashboard);
 router.get("/", ThoughtsController.showThoughts);
 
